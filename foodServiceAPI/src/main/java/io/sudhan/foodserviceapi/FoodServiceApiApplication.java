db5d0fcdd38de0d95843d3c4c0801bfc2e1ed51f
Arunagirinadan Sudharshan <sudhan.aruna.97@gmail.com>
2022-03-16 10:49:49 +0530

    SInitial commit with OAS

---
+++ foodServiceAPI/src/main/java/io/sudhan/foodserviceapi/FoodServiceApiApplication.java
@@ -0,0 +1,13 @@
+package io.sudhan.foodserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodServiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodServiceApiApplication.class, args);
+    }
+
+}
---
+++ foodServiceAPI/src/test/java/io/sudhan/foodserviceapi/FoodServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sudhan.foodserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodServiceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
