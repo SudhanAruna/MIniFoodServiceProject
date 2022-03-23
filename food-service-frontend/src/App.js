a6044c460f01ed480882a204031460faa6784013
Arunagirinadan Sudharshan <sudhan.aruna.97@gmail.com>
2022-03-23 13:09:37 +0530

    Initalized foodend project using Reactjs

---
+++ food-service-frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  
+  
+  
+  
+  return (
+    <div className='flex flex-col min-h-screen h-full'>
+      <p>start</p>
+    </div>
+  );
+}
+
+export default App;
