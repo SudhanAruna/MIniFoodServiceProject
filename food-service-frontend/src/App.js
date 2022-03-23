fb4dbbc287fcf43b9cf9c90ab707ea0550336854
Arunagirinadan Sudharshan <sudhan.aruna.97@gmail.com>
2022-03-23 13:29:31 +0530

    Component: Created home component and install router

--- food-service-frontend/src/App.js
+++ food-service-frontend/src/App.js
@@ -1,4 +1,5 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   
@@ -7,7 +8,14 @@ function App() {
   
   return (
     <div className='flex flex-col min-h-screen h-full'>
-      <p>start</p>
+      <BrowserRouter>
+      <div className='flex-grow flex-shrink-0 p-4'>
+            
+            <Routes>
+              <Route path="/" element={<Home/>} />
+            </Routes>
+          </div>
+      </BrowserRouter>
     </div>
   );
 }
